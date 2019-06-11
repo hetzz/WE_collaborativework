396beec6ba75df6f16ad7a18588d7bceae35c1a8
Bhumika Saxena <bhumika056btcse17@igdtuw.ac.in>
2019-06-11 05:01:03 +0000

    Update Ranklist.java

--- Ranklist.java
+++ Ranklist.java
@@ -2,8 +2,10 @@ package June11;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.io.*;
+import java.util.Collections;
 
 public class Ranklist {
+	
 	
 	public static void generateRankList() throws Exception {
 		
@@ -16,8 +18,22 @@ public class Ranklist {
 		ArrayList <Integer> markList = new ArrayList<Integer>();
 		ArrayList <String> nameList = new ArrayList<String>();
 		while ((string1 = br.readLine()) != null) {
+//			markList.add(Integer.parseInt(string1.substring(string1.length() - 3, string1.length() - 1)));
+//			nameList.add(string1.substring(0, 5));
 			rollNumberAndMarks.put(string1.substring(0, 5), Integer.parseInt(string1.substring(string1.length() - 3, string1.length() - 1)));
 		}
+		
+		HashMap<String, Integer> sorted = rollNumberAndMarks;
+		
+	    sorted = rollNumberAndMarks
+	            .entrySet()
+	            .stream()
+	            .sorted(Collections.reverseOrder(HashMap.Entry.comparingByValue()))
+	            .collect(
+	                toMap(HashMap.Entry::getKey, HashMap.Entry::getValue, (e1, e2) -> e2,
+	                    HashMap::new));
+		
+		
 	}
 
 	public static void main(String[] args) {
