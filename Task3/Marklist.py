6d4b9fa82a597dffdbe4af816525bd92e3d522d8
hetzz <hetal.kuvadia@somaiya.edu>
2019-06-12 19:27:53 +0530

    Modified Task3 Code

--- Task3/Marklist.py
+++ Task3/Marklist.py
@@ -3,9 +3,17 @@ def isgreater(student1 , student2):
     greaterThanCheck = sum(list(1 for i in range(3) if int(student1[i]) > int(student2[i]) ))
     return greaterThanCheck == 3
 
-# def checkTransitive(greaterThanPairs) :
-#     transitive = []
-#     for i in greaterThanPairs :
+def removeTransitive(greaterThanPairs) :
+    transitive_pairs = []
+    for pair1 in greaterThanPairs:
+        check_var = pair1[1]
+        for pair2 in greaterThanPairs :
+            if pair1 != pair2:
+                if pair2[0] == check_var:
+                    s = str(pair1[0]) + str(pair2[1])
+                    transitive_pairs.append(s)
+    return [pairs for pairs in greaterThanPairs if pairs not in transitive_pairs]
+
 
 def generateGreaterPairs(marksList):
     greaterThanPairs = []
@@ -22,6 +30,7 @@ def compareFunction() :
         marksList = list(marksFile.read().strip().split("\n"))
         marksList = [list(student.split(" "))for student in marksList]
         
-        return generateGreaterPairs(marksList)
+        generateThanPairs =  generateGreaterPairs(marksList)
+        return removeTransitive(generateThanPairs)
 
 print(compareFunction())
