f764e2c3b3cc31cef0be227628658756a8aeb6bb
Kanupriya Singh <kanupriya17294@iiitd.ac.in>
2019-06-12 05:19:49 +0000

    Updated odometer.py

--- Task2/odometer.py
+++ Task2/odometer.py
@@ -4,7 +4,7 @@ def initialise(num_digits):
 
 	for i in range(1, num_digits + 1):
 		num += str(i)
-	return (int(num))
+	return int(num)
 
 ######################################################################################################################
 
@@ -12,27 +12,27 @@ def largest_n_digit_num(num_digits):
 
 	largest_num = ''
 	start_digit = 9
-	
+
 	for i in range(num_digits, 0, -1):
 		largest_num += str(9)
 		start_digit -= 1
-	return (int(largest_num))
+	return int(largest_num)
 
 ######################################################################################################################
 
 def has_unique_digits(num):
 
-	return (len(set(num)) == len(num))
+	return len(set(num)) == len(num)
 
 ######################################################################################################################
 
 def isAscending(num):
 
-	return (num == ''.join(sorted(num)))
+	return num == ''.join(sorted(num))
 
 ######################################################################################################################
 
-def generate_order(num_digits):
+def generate_readings(num_digits):
 
 	initial_num = initialise(num_digits)
 	largest_num = largest_n_digit_num(num_digits)
@@ -41,31 +41,31 @@ def generate_order(num_digits):
 
 ######################################################################################################################
 
-def find_next_i(n, i, order):
+def find_next_i(reading, i, readings):
 
-	index = order.index(n)
-	return (order[index + i % len(order)]) if index != -1 else ("Not a valid reading")
+	index = readings.index(n)
+	return readings[index + i % len(readings)] if index != -1 else "Not a valid reading"
 
 ######################################################################################################################
 
-def find_prev_i(n, i, order):
+def find_prev_i(reading, i, readings):
 
-	index = order.index(n)
+	index = readings.index(n)
 	if (index - i < 0):
-		return (order[index - i + len(order)])
-	return (order[index - i]) if index != -1 else ("Not a valid reading")
+		return (readings[index - i + len(readings)])
+	return readings[index - i] if index != -1 else "Not a valid reading"
 
 ######################################################################################################################
 
-def find_difference(num1, num2, order):
+def find_difference(reading1, reading2, readings):
 
-	index1 = order.index(num1)
-	index2 = order.index(num2)
-	return abs(index1 - index2) if index2 > index1 else (len(order) - abs(index1 - index2))
+	index1 = readings.index(reading1)
+	index2 = readings.index(reading2)
+	return index2 - index1 if index2 > index1 else len(readings) - (index1 - index2)
 
 ######################################################################################################################
 
-order = generate_order(5)
-# print(find_next_i(45678, 3 ,order))
-# print(find_prev_i(12345, 1, order))
-# print(find_difference(46789, 45678, order))
+readings = generate_readings(5)
+# print(find_next_i(45678, 3 ,readings))
+# print(find_prev_i(12345, 1, readings))
+# print(find_difference(46789, 45678, readings))
