532954b598fd650bf6d3302cebcc30c574dac23e
Kanupriya Singh <kanupriya17294@iiitd.ac.in>
2019-06-12 04:18:06 +0000

    Updated odometer.py

--- Task2/odometer.py
+++ Task2/odometer.py
@@ -17,37 +17,53 @@ def largest_n_digit_num(num_digits):
 
 ###########################################################
 
+def has_same_digts(num):
+
+	for i in range(len(num) - 1):
+		if num[i] == num[i+1]:
+			return False
+	return True
+
+###########################################################
+
 def generate_order(num_digits):
 
 	order = []
 	initial_num = initialise(num_digits)
 	largest_num = largest_n_digit_num(num_digits)
 	for i in range(initial_num, largest_num):
-		if(str(i) == ''.join(sorted(str(i)))):
+		if(str(i) == ''.join(sorted(str(i))) and has_same_digts(str(i))):
 			order.append(i)
+	print(order)
 	return order
 
 ###########################################################
 
 def find_next_i(n, i, order):
-	ind = order.index(n)
-	return (order[ind + i])
+
+	index = order.index(n)
+	return (order[index + i % len(order)])
 
 ###########################################################
 
 def find_prev_i(n, i, order):
-	ind = order.index(n)
-	return (order[ind - i])
+
+	index = order.index(n)
+	if (index - i < 0):
+		return (order[index - i + len(order)])
+	return (order[index - i])
 
 ###########################################################
 
 def find_difference(num1, num2, order):
+
 	index1 = order.index(num1)
 	index2 = order.index(num2)
-	return (abs(index1 - index2))
+	return abs(index1 - index2) if index2 > index1 else (len(order) - abs(index1 - index2))
 
 ###########################################################
 
 order = generate_order(5)
 print(find_next_i(45678, 3 ,order))
-print(find_difference(36789, 45678, order))
+print(find_prev_i(12345, 1, order))
+print(find_difference(46789, 45678, order))
