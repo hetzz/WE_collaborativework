bccd3ea0388ff2b9ce39766f48ca2f68feca62fb
hetzz <hetal.kuvadia@somaiya.edu>
2019-06-20 13:39:14 +0530

    Game Strategies

--- Task8/Diamonds.py
+++ Task8/Diamonds.py
@@ -1,43 +1,48 @@
 import random
+
 class Play :
     def __init__(self):
         self.player = [i for i in range(2,15)]
         self.computer = [i for i in range(2,15)]
         self.diamonds = [i for i in range(2,15)]
-        self.score_player = 0
-        self.score_computer = 0
+        random.shuffle(self.diamonds)
+        self.player_score = 0
+        self.computer_score = 0
 
-    def generateDiamond_card(self):
-	    return self.diamonds[random.randrange(0, len(self.diamonds))]
+    def strategyRandom(self):
+        return self.computer.pop()
 
-    def generateComputer_card(self):
-		return self.computer[random.randrange(0, len(self.computer))]
+    def strategyAdd2(self):
+        randomIndex = random.randrange(0, len(self.diamonds))
+        diamondCard = self.diamonds[randomIndex]
+        return self.computer[(randomIndex + 2) % len(self.computer)] , diamondCard
 
     def gamePlay(self):
+        random.shuffle(self.computer)
         for i in range(13) :
             if len(self.diamonds) == 0 :
                 print("Game Over")
                 break
-            computer_card = self.generateComputer_card()
-            diamond_card = self.generateDiamond_card()
-            player_card = int(input())
-            print(computer_card ,'\t',player_card)
-            print("Score :" , self.score_computer, self.score_player)
-            self.Banker(diamond_card, player_card, computer_card)
+            computerbid = self.computer.pop()
+            diamond_card = self.diamonds.pop()
+            playerbid = int(input('Enter card : '))
+            computerbid, diamond_card = self.strategyAdd2()
+            print('Computer : ',computerbid ,'\t',playerbid, '\t', diamond_card)
+            self.Banker(diamond_card, playerbid, computerbid)
+            print("Score :" , self.computer_score, self.player_score)
 
     def showState(self):
-        print(self.player, self.computer, self.diamonds, self.score_player, self.score_computer)
+        print(self.player, self.computer, self.diamonds, self.player_score, self.computer_score)
 
-    def Banker(self, diamond_card , player_card , computer_no):
-        if player_card == computer_no :
-            self.score_player, self.score_computer = diamond_card / 2, diamond_card / 2
-        elif player_card > computer_no :
-            self.score_player += diamond_card
+    def Banker(self, diamond_card , playerbid , computerbid):
+        if playerbid == computerbid :
+            self.player_score += diamond_card / 2
+            self.computer_score += diamond_card / 2
+        elif playerbid > computerbid :
+            self.player_score += diamond_card
         else :
-            self.score_computer += diamond_card
-        self.diamonds.remove(diamond_card)
-        self.player.remove(player_card)
-        self.computer.remove(computer_no)
+            self.computer_score += diamond_card
+        self.player.remove(playerbid)
 
 game = Play()
 game.gamePlay()
