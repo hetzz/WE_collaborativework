ed6f1649e0919ee30b4bf4abb29d9aa46bd45133
hetzz <hetal.kuvadia@somaiya.edu>
2019-06-20 14:44:28 +0530

    Cleaned  Diamonds.py

--- Task8/Diamonds.py
+++ Task8/Diamonds.py
@@ -3,6 +3,7 @@ import random
 class Play :
     def __init__(self):
         self.player = [i for i in range(2,15)]
+        #self.computer = random.shuffle([i for i in range(2,15)])
         self.computer = [i for i in range(2,15)]
         self.diamonds = [i for i in range(2,15)]
         random.shuffle(self.diamonds)
@@ -10,29 +11,15 @@ class Play :
         self.computer_score = 0
 
     def strategyRandom(self):
-        return self.computer.pop()
+        return self.computer.pop() , self.diamonds.pop()
 
     def strategyAdd2(self):
         randomIndex = random.randrange(0, len(self.diamonds))
         diamondCard = self.diamonds[randomIndex]
         return self.computer[(randomIndex + 2) % len(self.computer)] , diamondCard
 
-    def gamePlay(self):
-        random.shuffle(self.computer)
-        for i in range(13) :
-            if len(self.diamonds) == 0 :
-                print("Game Over")
-                break
-            computerbid = self.computer.pop()
-            diamond_card = self.diamonds.pop()
-            playerbid = int(input('Enter card : '))
-            computerbid, diamond_card = self.strategyAdd2()
-            print('Computer : ',computerbid ,'\t',playerbid, '\t', diamond_card)
-            self.Banker(diamond_card, playerbid, computerbid)
-            print("Score :" , self.computer_score, self.player_score)
-
-    def showState(self):
-        print(self.player, self.computer, self.diamonds, self.player_score, self.computer_score)
+    def showState(self,playerbid,computerbid,diamond_card):
+        print("PlayerCard :",playerbid,"ComputerCard", computerbid,"Diamond :", diamond_card,"PlayerScore :" ,self.player_score,"ComputerScore", self.computer_score)
 
     def Banker(self, diamond_card , playerbid , computerbid):
         if playerbid == computerbid :
@@ -44,5 +31,22 @@ class Play :
             self.computer_score += diamond_card
         self.player.remove(playerbid)
 
-game = Play()
-game.gamePlay()
+    def gamePlay(self):
+        for i in range(13) :
+            if len(self.diamonds) == 0 :
+                print("Game Over")
+                break
+            playerbid = int(input('Enter card : '))
+            computerbid, diamond_card = self.strategyAdd2()
+            self.Banker(diamond_card, playerbid, computerbid)
+            self.showState(playerbid,computerbid,diamond_card)
+
+
+GAME =True
+while GAME :
+    game = Play()
+    game.gamePlay()
+    print("\n\nDo you want to Continue(y/n)?")
+    choice = input()
+    if(choice.lower() != 'y'):
+        break
